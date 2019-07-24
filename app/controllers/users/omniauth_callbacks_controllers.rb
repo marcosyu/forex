1d4b9b10b1070999a5d3efd645df11e9ef5d8f33
marcosyu <marcosyu26@gmail.com>
2019-07-24 12:16:44 -0700

    fix redirect

--- app/controllers/users/omniauth_callbacks_controllers.rb
+++ app/controllers/users/omniauth_callbacks_controllers.rb
@@ -6,10 +6,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
-      sign_in_and_redirect @user, event: :authentication
+      sign_in_and_redirect collections_path, event: :authentication
     else
       session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
-      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+      redirect_to root_path, alert: @user.errors.full_messages.join("\n")
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
 
-  resource :user do
-    resources :calculations
-  end
+  resources :calculations
 
   root to: 'home#index'
 end
