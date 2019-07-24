628eb5d92d955c4164dde22036aa9354b112583b
marcosyu <marcosyu26@gmail.com>
2019-07-24 12:38:19 -0700

    added email

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :email, :provider, :uid, presence: true
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    where(email: auth.email, provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.token = auth.credentials.token
       user.expires = auth.credentials.expires
       user.expires_at = auth.credentials.expires_at
