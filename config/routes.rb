9bd9fc4957d3358e13a8b32171a41a6f36f3e160
marcosyu <marcosyu26@gmail.com>
2019-07-24 23:54:06 -0700

    route for sign_out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  get 'signout' => 'devise/sessions#destroy', :as => :destroy_user_session
+  delete 'signout' => 'devise/sessions#destroy', :as => :destroy_user_session
 
   namespace :admin do
     resources :calculations
