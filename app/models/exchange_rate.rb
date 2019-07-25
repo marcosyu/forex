11ca84eb707e8d64a5bd8f661fd12089cd4ee7a1
marcosyu <marcosyu26@gmail.com>
2019-07-25 03:58:33 -0700

    moved to model

--- app/models/exchange_rate.rb
+++ app/models/exchange_rate.rb
@@ -2,4 +2,10 @@ class ExchangeRate < ApplicationRecord
   belongs_to :user
   validates :amount, :base_currency, :target_currency, presence: true
   serialize :historical_duration, Hash
+
+  after_save :pull_history
+
+  def pull_history
+    ExchangeRateServices.new(self.id).get_histories
+  end
 end
