797d6abcd923dc92d29373814f9477b12fc90f3f
marcosyu <marcosyu26@gmail.com>
2019-07-24 23:43:39 -0700

    exchange rate temp file

--- app/services/exchange_rate_services.rb
+++ app/services/exchange_rate_services.rb
@@ -7,16 +7,28 @@ class ExchangeRateServices
   end
 
   def get_histories
+    tmp_file = Rails.root.join('tmp/storage/histogram.json')
 
-    params = {
-      access_key: ENV["CURRENCY_API_KEY"],
-      start_date: 25.weeks.ago.strftime("%Y-%m-%d"),
-      end_date: Date.today.strftime("%Y-%m-%d"),
-      base: @@base_currency,
-      symbols: @@symbols
-    }
+    if File.exist?(tmp_file)
+      params = {
+        access_key: ENV["CURRENCY_API_KEY"],
+        base: @@base_currency,
+        symbols: @@symbols
+      }
+      data = JSON.parse(File.read(tmp_file))[@@exchange_rate.id]
 
-    response = Faraday.get("#{ENV["CURRENCY_URL"]}/timeseries", params)
+      (30.days.ago.to_date..Date.today).each do |date|
+        if !data.keys.include? date
+          response = Faraday.get("#{ENV['CURRENCY_URL']}/${date.strftime('%Y-%m-%d')}", params)
+          data.merge!(JSON.parse(response.body)["rates"])
+        end
+      end
+    else
+      histogram = JSON.parse(response.body)["rates"]
+      rate = {}
+      rate[@@exchange_rate.id]= histogram
+      File.open(Rails.root.join('tmp/storage/currencies.json'),'w+'){|f| f.write rate.to_json }
+    end
 
     ## no need to cache the data since this would be done in the background job
     @@exchange_rate.update_attribute('historical_duration', JSON.parse(response.body)["rates"] )
@@ -36,12 +48,10 @@ class ExchangeRateServices
         return JSON.parse(File.read(file_url))
       else
         response = Faraday.get("#{ENV["CURRENCY_URL"]}/latest", { access_key: ENV["CURRENCY_API_KEY"] })
-
         data = JSON.parse(response.body)
 
         ##catch the error
-
-        if data[:success]
+        if data["success"]
           currencies = JSON.parse(response.body)["rates"].keys
           File.open(Rails.root.join('tmp/storage/currencies.json'),'w'){|f| f.write currencies.to_json }
           return currencies
