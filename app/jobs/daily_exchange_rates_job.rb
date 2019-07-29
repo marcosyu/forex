7ca17886e9d4e0b1a4eec91a355abc9f2dc75878
marcosyu <marcosyu26@gmail.com>
2019-07-29 00:14:31 -0700

    add provider on job

--- app/jobs/daily_exchange_rates_job.rb
+++ app/jobs/daily_exchange_rates_job.rb
@@ -1,7 +1,7 @@
 class DailyExchangeRatesJob < ApplicationJob
   queue_as :daily_exchange_rates_job
 
-  def perform(provider)
-    ExchangeRateServices.new({provider: provider, action:'latest'}).call
+  def perform(args)
+    ExchangeRateServices.new({provider: args['provider'], action:'latest'}).call
   end
 end
