39648c5a5079f1ae9dae2eac40117df99c048448
marcosyu <marcosyu26@gmail.com>
2019-07-28 20:44:14 -0700

    update for mapping of value pairs for daily update

--- app/services/exchange_rate_apis/fixer_api_service.rb
+++ app/services/exchange_rate_apis/fixer_api_service.rb
@@ -1,46 +1,69 @@
 class ExchangeRateApis::FixerApiService
 
   def initialize args={}
-    @start = 25.days.ago.to_date
-    @end = Date.today
-    @base = args[:base]
-    @symbols = args[:symbols]
-    @action = args[:action]
+    @attributes = args
   end
 
   def call
     data = fetch_from_fixer
-    data_to_value_series data
+    if data.class == Array
+      mapped_data = data.map{|d| data_to_value_series d }
+    else
+      mapped_data = data_to_value_series data
+    end
+    return mapped_data.class == Array ? mapped_data.join(',') : mapped_data
   end
 
   private
 
   def fetch_from_fixer
-    response = Faraday.get(url_for_action, params_for_action)
-    JSON.parse(response.body)
+    params = params_for_action
+    if params.class == Array
+      data = []
+      params.each do |param|
+        response = Faraday.get(url_for_action, param)
+        data << JSON.parse(response.body)
+      end
+      Rails.logger.warn "call count #{data.count}"
+    else
+      response = Faraday.get(url_for_action, params)
+      data = JSON.parse(response.body)
+    end
+    data
   end
 
   def url_for_action
-    case action
+    case @attributes[:action]
     when 'latest'
-      return "#{ENV['FIXER_URL']}/latest"
-      # http://data.fixer.io/api/latest
+      return "#{ENV['FIXER_API_URL']}/latest"
     when 'by_date'
-      return "#{ENV['FIXER_URL']}/#{@start}"
+      return "#{ENV['FIXER_API_URL']}/#{@start}"
       # http://data.fixer.io/api/2013-12-24
     when 'histories'
-      return "#{ENV['FIXER_URL']}/timeseries"
+      return "#{ENV['FIXER_API_URL']}/timeseries"
       # http://data.fixer.io/api/timeseries
     end
   end
 
   def params_for_action
-    params ={
-      access_key: ENV['FIXER_API_KEY'],
-      base: @base,
-      symbols: @symbols.join(',')
-    }
-    params!.merge({start: @start, end: @end}) if @action == 'histories'
+    if @attributes[:currency_pairs].present?
+      params = []
+      @attributes[:currency_pairs].keys.each do |key|
+        params << {
+          access_key: ENV['FIXER_API_KEY'],
+          # base: key,
+          symbols: @attributes[:currency_pairs][key].join(',')
+        }
+      end
+    else
+      params ={
+        access_key: ENV['FIXER_API_KEY'],
+        # base: @attributes[:base],
+        symbols: @attributes[:symbols].join(',')
+      }
+      params!.merge({start: @attributes[:start].strftime('%Y-%m-%d'), end: @attributes[:end].strftime('%Y-%m-%d')}) if @attributes[:action] == 'histories'
+    end
+    params
   end
 
   def data_to_value_series data
--- app/services/exchange_rate_services.rb
+++ app/services/exchange_rate_services.rb
@@ -2,20 +2,18 @@ class ExchangeRateServices
 
   def initialize args={}
     @attributes = args
+
     if args[:action] == 'histories' && args[:favorite_exchange_rate_id].present?
       exchange_rate = FavoriteExchangeRate.find(args[:favorite_exchange_rate_id])
       @attributes[:base] = exchange_rate.base_currency
       @attributes[:symbols] = exchange_rate.target_currency
+    elsif args[:action] == 'latest'
+      @attributes[:currency_pairs] = currency_pairs
     end
   end
 
   def call
-    values = []
-    case @provider
-    when 'fixer_api'
-      values += ExchangeRateApis::FixerApiService.new(@attributes.except(:provider)).call
-    end
-
+    values = get_values_from_api
     if values.present?
       begin
         sql = "INSERT INTO exchange_rates (base_currency, target_currency, date, rate, created_at, updated_at) VALUES #{values.join(', ')}"
@@ -27,4 +25,20 @@ class ExchangeRateServices
 
   end
 
+  private
+
+  def get_values_from_api
+    data = []
+    case @provider
+    when 'fixer_api'
+      data << ExchangeRateApis::FixerApiService.new(@attributes.except(:provider)).call
+    end
+    data.class == Array ? data.join(',') : data
+  end
+
+
+  def currency_pairs
+    rates = FavoriteExchangeRate.all.map{|rate| [rate.base_currency, rate.target_currency] }
+    rates.group_by(&:first).transform_values { |v| v.map(&:last) }
+  end
 end
